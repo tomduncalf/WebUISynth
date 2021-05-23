9f4491543c9aff43d57aae788a897f5ca8761332
Tom Duncalf <tom@tomduncalf.com>
2021-05-23 16:02:01 +0100

    WIP on slider style

--- ui/src/components/ParameterSlider.tsx
+++ ui/src/components/ParameterSlider.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react";
 import { FC } from "react";
+import styled, { css } from "styled-components";
 import { ParameterModel } from "../juceIntegration/models/ParameterModel";
 
 interface Props {
@@ -10,11 +11,126 @@ interface Props {
   parameter: ParameterModel<number>;
 }
 
+const trackH = "0.4em";
+const thumbD = "1.5em";
+const trackC = "#ccced0";
+const filllC = "#ffc069";
+
+const track = css`
+  box-sizing: border-box;
+  border: none;
+  height: 4px;
+  background: ${trackC};
+  border-radius: 8px;
+`;
+
+const trackFill = css`
+  ${track};
+  height: 6px;
+  background-color: transparent;
+  background-image: linear-gradient(${filllC}, ${filllC}),
+    linear-gradient(${trackC}, ${trackC});
+  background-size: var(--sx) 6px, calc(100% - var(--sx)) 4px;
+  background-position: left center, right center;
+  background-repeat: no-repeat;
+`;
+
+const fill = css`
+  height: ${trackH};
+  background: ${filllC};
+  border-radius: 4px;
+`;
+
+const thumb = css`
+  box-sizing: border-box;
+  border: none;
+  width: ${thumbD};
+  height: ${thumbD};
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0px 0px 5px rgba(66, 97, 255, 0.5);
+`;
+
+const Input = styled.input`
+  &,
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus::-webkit-slider-thumb {
+    outline: -webkit-focus-ring-color auto 5px;
+  }
+
+  &:focus::-moz-range-thumb {
+    outline: -webkit-focus-ring-color auto 5px;
+  }
+
+  &:focus::-ms-thumb {
+    outline: -webkit-focus-ring-color auto 5px;
+  }
+
+  --range: calc(var(--max) - var(--min));
+  --ratio: calc((var(--val) - var(--min)) / var(--range));
+  --sx: calc(0.5 * ${thumbD} + var(--ratio) * (100% - ${thumbD}));
+
+  margin: 0;
+  padding: 0;
+  height: ${thumbD};
+  background: transparent;
+  font: 1em/1 arial, sans-serif;
+
+  &::-webkit-slider-runnable-track {
+    ${trackFill};
+  }
+
+  &::-moz-range-track {
+    ${track};
+  }
+
+  &::-ms-track {
+    ${track};
+  }
+
+  &::-moz-range-progress {
+    ${fill};
+  }
+
+  &::-ms-fill-lower {
+    ${fill};
+  }
+
+  &::-webkit-slider-thumb {
+    margin-top: calc(0.5 * (${trackH} - ${thumbD}));
+    ${thumb};
+  }
+
+  &::-moz-range-thumb {
+    ${thumb};
+  }
+
+  &::-ms-thumb {
+    margin-top: 0;
+    ${thumb};
+  }
+
+  &::-ms-tooltip {
+    display: none;
+  }
+
+  &::-moz-focus-outer {
+    border: 0;
+  }
+`;
+
 export const ParameterSlider: FC<Props> = observer((props) => {
   return props.parameter ? (
     <div style={{ display: "flex", marginBottom: 2 }}>
       <div style={{ flex: 1 }}>{props.label}</div>
-      <input
+      <Input
         type="range"
         min={props.min || 0}
         max={props.max || 1}
